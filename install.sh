ae6f08e0058a2a40702755ed0ebd8f53a8a56cb3
George Burton <burtonageo@gmail.com>
2014-09-06 00:24:51 +0100

    Added more install script stuff

--- install.sh
+++ install.sh
@@ -11,27 +11,30 @@ mkdir -p ~/Projects
 mkdir -p ~/Projects/CPP
 mkdir -p ~/Projects/Haskell
 mkdir -p ~/Projects/Rust
+mkdir -p ~/Projects/ObjC
 mkdir -p ~/Projects/exercism
 
 # Get Xcode
 xcode-select --install &&
 
+# Get Alcatraz package manager
+curl -fsSL https://raw.github.com/supermarin/Alcatraz/master/Scripts/install.sh | sh &&
+
 # Download homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" &&
 
 # My cli apps
-"brew install python3 haskell-platform wget sl git bash wine mit-scheme pandoc
-              coq figlet cowsay cmake doxygen xctool gcc49 ddate proof-general
-              tor tree exercism rbenv ack emacs vim mono" &&
+"brew install python3 haskell-platform wget sl git bash wine chibi-scheme pandoc
+              figlet cowsay cmake doxygen xctool gcc49 ddate tor tree exercism
+              rbenv ack emacs vim mono mercurial cmake" &&
 
 # My general apps
 brew install caskroom/cask/brew-cask &&
 "brew cask install plug sonora qqmusic appcleaner texshop
                    selfcontrol blender chocolat anki alfred
-                   transmission the-unarchiver archive-browser
-                   aquamacs macvim steam vlc flux dropbox caffeine
-                   skype handbrake xamarin-studio unity3d
-                   google-chrome firefox deathtodsstore"
+                   transmission the-unarchiver aquamacs macvim
+                   steam vlc flux dropbox caffeine skype handbrake
+                   java google-chrome deathtodsstore hex-fiend"
 
 # Get rust
-rget
+rinstall
